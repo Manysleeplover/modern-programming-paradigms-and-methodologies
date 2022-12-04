7d8ff3c4a4e5d36408733a9fc0afc1ae25069762
ZhannaIos <zhanna.isf@yandex.ru>
2022-12-04 12:29:55 +0300

    Финалочка.

--- Лабораторная работа №1/src/main/java/ru/romanov/statkin/lab1/Lab1/Lab1Application.java
+++ Лабораторная работа №1/src/main/java/ru/romanov/statkin/lab1/Lab1/Lab1Application.java
@@ -13,38 +13,53 @@ import java.util.stream.Stream;
  * массивов и выбрасывающую ключи с последовательно повторяющимися значениями
  */
 public class Lab1Application {
+    /**
+     * Инициализируем списки, вызываем целевой метод
+     */
     public static void main(String[] args) {
-        List<ImmutablePair<Integer, String>> list = new ArrayList<>(Stream.of(
-                new ImmutablePair<Integer, String>(1, "a"),
-                new ImmutablePair<Integer, String>(2, "a"),
-                new ImmutablePair<Integer, String>(3, "a"),
-                new ImmutablePair<Integer, String>(4, "b"),
-                new ImmutablePair<Integer, String>(5, "b"),
-                new ImmutablePair<Integer, String>(6, "a"),
-                new ImmutablePair<Integer, String>(7, "b"),
-                new ImmutablePair<Integer, String>(8, "a")).toList());
-        System.out.print(list);
+        //Метод, генерирующий список пар<Число, Строка> - где ключ последовательность от 1 до бесконечности, а значение либо "а" либо "б"
+        //Ограничен 10 элементами
+        List<ImmutablePair<Integer, String>> list = Stream.iterate(0, i -> i + 1).sequential().limit(10)
+                .map(i -> new ImmutablePair<>(i + 1, Math.random() >= 0.5 ? "a" : "b")).toList();
+
         List<Integer> integers = new ArrayList<>();
-        System.out.print(getKeysOfDuplicateValues(list, 0, integers));
+
+        System.out.println("Список повторяющихся ключей до выполнения метода" + integers);
+        System.out.println("Исходный второй ассоциативный массив: " + list);
+        System.out.println("Результат выполнения метода" + getKeysOfDuplicateValues(list, 0, integers));
     }
 
+    /**
+     * Метод, возвращающий первое вхождение повторяющегося элемента.
+     * То есть если мы имеем (1,a),(2,a),(3,a)(4,b), то он выведет (1,2)
+     * @param list - бесконечный ассоциативный массив
+     * @param startIndex - индекс, на котором закончилась предыдущая итерация
+     * @param integers - список с ключами повторяющихся значений
+     */
     public static List<Integer> getKeysOfDuplicateValues(List<ImmutablePair<Integer, String>> list,
                                                          int startIndex,
                                                          List<Integer> integers) {
+        //Если список пустой, то ничего не делает
         if (list.isEmpty()) {
             return integers;
         }
+        //Если остался один элемент, то тоже ничего не делаем
         if ((list.size() - startIndex) == 1) {
             return integers;
         }
 
+        //Получаем иммутабельные пары для сравнения
         ImmutablePair<Integer, String> _first = list.get(startIndex);
         ImmutablePair<Integer, String> _second = list.get(startIndex + 1);
+
+        //Собсна сравниваем
+        //Если пары равны, передаем в следующую итерацию список с ключом
         if (_first.right.equals(_second.right)) {
             return (getKeysOfDuplicateValues(list,
                     startIndex + 1, Stream.of(integers, Arrays.asList(_first.left))
                             .flatMap(Collection::stream)
                             .collect(Collectors.toList())));
+        // На нет и суда нет.
         } else {
             return (getKeysOfDuplicateValues(list,
                     startIndex + 1, integers));
